8a4f3bcfc5b69b886f3f29e5e11676bfaf3d5bce
Lucaz2010 <oliveirasantos.lucas.sm@gmail.com>
2025-03-18 21:14:49 +0100

    UUID Generator added

---
+++ src/main/java/easv/dk/eventticketsystem/bll/UUIDGenerator.java
@@ -0,0 +1,46 @@
+package easv.dk.eventticketsystem.bll;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitArray;
+import com.google.zxing.common.BitMatrix;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.util.UUID;
+
+public class UUIDGenerator {
+    private void GenerateUUID(){
+        try {
+            // Generate a random UUID
+            String uuid = UUID.randomUUID().toString();
+            System.out.println("Generated UUID: " + uuid);
+
+            // Set file path for the QR Code image
+            String filePath = "uuid_qr.png";
+
+            // Generate and save the QR Code
+            generateQRCode(uuid, filePath, 300, 300);
+            System.out.println("QR Code generated and saved as: " + filePath);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void startQRCodeGeneration() {
+        GenerateUUID();
+    }
+
+    public static void generateQRCode(String data, String filePath, int width, int height) throws WriterException, IOException {
+        BitMatrix matrix = new MultiFormatWriter().encode(data, BarcodeFormat.QR_CODE, width, height);
+        Path path = FileSystems.getDefault().getPath(filePath);
+        MatrixToImageWriter.writeToPath(matrix, "PNG", path);
+    }
+}
+
