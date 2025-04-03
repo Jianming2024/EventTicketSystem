c8181c2278d323cb982ca75a68974f66107db727
Jianming Fan <151937346+Jianming2024@users.noreply.github.com>
2025-04-03 14:01:29 +0200

    change icons on sidebar

---
+++ src/main/java/easv/dk/eventticketsystem/bll/util/SearchManager.java
@@ -0,0 +1,5 @@
+package easv.dk.eventticketsystem.bll.util;
+
+public class SearchManager {
+
+}
--- src/main/java/easv/dk/eventticketsystem/gui/controllers/ToolbarController.java
+++ src/main/java/easv/dk/eventticketsystem/gui/controllers/ToolbarController.java
@@ -1,4 +1,18 @@
 package easv.dk.eventticketsystem.gui.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class ToolbarController {
+    @FXML
+    private TextField txtQuery;
+    @FXML
+    private Label lblUserName;
+
+    public void handleSearch(ActionEvent actionEvent) {
+        String query = txtQuery.getText();
+        System.out.println("Searching for: " + query);
+    }
 }
